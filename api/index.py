f624eabf052ff0d050ee1f423d16a0bc25215eae
eliranna <eliran.natan.87@gmail.com>
2023-06-04 11:17:13 +0000

    init

--- api/index.py
+++ api/index.py
@@ -2,10 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return 'Hello, World!'
+  return 'Hello, World2!'
+
 
 @app.route('/about')
 def about():
-    return 'About'
+  return 'About2'
+
+
+app.run(host='0.0.0.0', port=81)
